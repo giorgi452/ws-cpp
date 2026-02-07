068160f55b5d3edd8caf93c6c933f604c6477b67
Giorgi Milashvili <giorgi.milashvili10@gmail.com>
2026-02-07 20:26:37 +0400

    working router

--- include/routes.hpp
+++ include/routes.hpp
@@ -1,15 +1,7 @@
 #pragma once
+#include "router.hpp"
 
-#include "http_request.hpp"
-#include <functional>
-#include <map>
-#include <string>
-#include <utility>
-
-using Handler = std::function<std::string(const HttpRequest &)>;
-
-using RouteKey = std::pair<std::string, std::string>;
-
-using RouteMap = std::map<RouteKey, Handler>;
-
-void setup_routes(RouteMap &routes);
+class Routes {
+public:
+  void setup(RouteMap &routes);
+};
