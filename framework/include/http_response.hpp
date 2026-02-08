d0a6777779620de210b2905691560aa71bb33606
Giorgi Milashvili <giorgi.milashvili10@gmail.com>
2026-02-08 20:06:07 +0400

    added json support

--- framework/include/http_response.hpp
+++ framework/include/http_response.hpp
@@ -1,37 +1,28 @@
 #pragma once
 
 #include <map>
+#include <nlohmann/json.hpp>
+#include <sstream>
 #include <string>
-#include <vector>
 
-/**
- * HttpResponse Class
- * Handles the construction of a valid HTTP/1.1 response string.
- */
 class HttpResponse {
 public:
-  // Basic HTTP components
   int status_code = 200;
   std::string status_message = "OK";
   std::string body;
   std::map<std::string, std::string> headers;
 
-  /**
-   * Sets the response body and automatically calculates the Content-Length
-   * header. Defaults to text/plain if no content_type is provided.
-   */
   void set_body(const std::string &content,
                 const std::string &content_type = "text/plain") {
     body = content;
     headers["Content-Type"] = content_type;
-    // The Content-Length is essential for the client to know when the message
-    // ends
     headers["Content-Length"] = std::to_string(body.length());
   }
 
-  /**
-   * Helper to set common status codes quickly
-   */
+  void set_json(const nlohmann::json &j) {
+    set_body(j.dump(4), "application/json");
+  }
+
   void set_status(int code) {
     status_code = code;
     switch (code) {
@@ -56,34 +47,19 @@ public:
     }
   }
 
-  /**
-   * Converts the object into a raw string ready to be sent over a socket.
-   * Ensures RFC-compliant CRLF (\r\n) line endings.
-   */
   std::string to_string() {
-    // 1. Start with the Status Line
-    std::string res = "HTTP/1.1 " + std::to_string(status_code) + " " +
-                      status_message + "\r\n";
+    std::ostringstream oss;
 
-    // 2. Add default headers if they haven't been set manually
-    if (headers.find("Server") == headers.end()) {
-      headers["Server"] = "WebServer++/1.0";
-    }
-    if (headers.find("Connection") == headers.end()) {
-      headers["Connection"] = "close";
-    }
+    oss << "HTTP/1.1 " << status_code << " " << status_message << "\r\n";
 
-    // 3. Append all headers
     for (auto const &[key, val] : headers) {
-      res += key + ": " + val + "\r\n";
+      oss << key << ": " << val << "\r\n";
     }
 
-    // 4. The Critical Blank Line (separates headers from body)
-    res += "\r\n";
+    oss << "\r\n";
 
-    // 5. Append the body content
-    res += body;
+    oss << body;
 
-    return res;
+    return oss.str();
   }
 };
